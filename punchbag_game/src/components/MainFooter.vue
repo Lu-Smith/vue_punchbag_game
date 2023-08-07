ff7edd9f4f094cc29ee36b79ebcfc3789e057ff4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-07 22:59:09 +0100

    Updated footer testing

--- punchbag_game/src/components/MainFooter.vue
+++ punchbag_game/src/components/MainFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="Footer">
-    <h2>Footer</h2>
+    <h4>This game was coded by 
+      <a class="profile-link" href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a> 
+      and is available as an open-source project on 
+      <a class="gitHub-link" href="https://github.com/Lu-Smith/rock_paper_scisors_vue_typescript_tdd" target="_blank">GitHub</a>.
+    </h4>
   </div>
 
 </template>
-
-<script lang="ts" setup>
-
-</script>
 
 <style>
 .Footer {
---
+++ punchbag_game/tests/unit/footer.spec.ts
@@ -0,0 +1,27 @@
+import MainFooterVue from "@/components/MainFooter.vue";
+import { shallowMount } from "@vue/test-utils";
+
+describe('MainFooter', () => {
+    it('renders all elements correctly', () => {
+        const wrapper = shallowMount(MainFooterVue)
+
+        const textElement = wrapper.find('h4')
+        expect(textElement.exists()).toBe(true)
+        expect(textElement.text()).toBe('This game was coded by Luna Smith and is available as an open-source project on GitHub.')
+        
+        
+         //profile link
+         const profileLink = textElement.find('a.profile-link')
+         expect(profileLink.exists()).toBe(true)
+         expect(profileLink.text()).toBe('Luna Smith')
+         expect(profileLink.attributes('href')).toBe('https://www.lunasmithart.com/')
+         expect(profileLink.attributes('target')).toBe('_blank')
+        
+         //gitHub link
+         const gitHubLink = textElement.find('a.gitHub-link')
+         expect(gitHubLink.exists()).toBe(true)
+         expect(gitHubLink.text()).toBe('GitHub')
+         expect(gitHubLink.attributes('href')).toBe('https://github.com/Lu-Smith/rock_paper_scisors_vue_typescript_tdd')
+         expect(gitHubLink.attributes('target')).toBe('_blank')
+    })
+})
