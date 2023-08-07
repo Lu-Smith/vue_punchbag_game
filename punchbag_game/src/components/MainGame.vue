da699aefa5179d39946eee53e42e37b27a865de4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-07 20:06:53 +0100

    Updated MainGame

--- punchbag_game/src/components/MainGame.vue
+++ punchbag_game/src/components/MainGame.vue
@@ -2,8 +2,10 @@
   <div>
     <h2>Main Game</h2>
     <div id="bag"> bag image</div>
-    <div>bag health</div>
-    <div>game controls</div>
+    <div id="bag-health">bag health</div>
+    <div id="controls">
+      <button>Punch</button>
+    </div>
   </div>
 </template>
 
