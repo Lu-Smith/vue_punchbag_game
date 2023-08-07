4a1361c2c2a8c284322d8f26343f258e833e4a01
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-07 17:51:03 +0100

    Updated App.vue testing

--- punchbag_game/tests/unit/example.spec.ts
+++ punchbag_game/tests/unit/example.spec.ts
@@ -1,12 +1,21 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '../../src/App.vue'
 import MainFooter from '../../src/components/MainFooter.vue'
+import MainGame from '../../src/components/MainGame.vue'
+import MainHeader from '../../src/components/MainHeader.vue'
 
 
 describe('App.vue', () => {
   it('renders all elements', () => {
     const wrapper = shallowMount(App)
+
     const footerComponent = wrapper.findComponent(MainFooter)
     expect(footerComponent.exists()).toBe(true)
+
+    const headerComponent = wrapper.findComponent(MainHeader)
+    expect(headerComponent.exists()).toBe(true)
+
+    const mainComponent = wrapper.findComponent(MainGame)
+    expect(mainComponent.exists()).toBe(true)
   })
 })
