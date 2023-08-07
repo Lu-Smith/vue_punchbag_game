ee9795a9a8e0fec68e16e8c5a679dd51e27e183f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-07 20:23:09 +0100

    Updated MainGame

--- punchbag_game/src/components/MainGame.vue
+++ punchbag_game/src/components/MainGame.vue
@@ -2,7 +2,9 @@
   <div>
     <h2>Main Game</h2>
     <div id="bag"> bag image</div>
-    <div id="bag-health">bag health</div>
+    <div id="bag-health">
+      <div></div>
+    </div>
     <div id="controls">
       <button>Punch</button>
     </div>
@@ -21,4 +23,17 @@
     background: url(../images/punchbagMain.png) center no-repeat;
     background-size: 200%;
   }
+
+  #bag-health {
+    width: 200px;
+    border: 2px solid #000;
+    margin: 0 auto 20px auto;
+  }
+
+  #bag-health div {
+    height: 20px;
+    background: rgb(241, 10, 10);
+  }
+
+
 </style>
