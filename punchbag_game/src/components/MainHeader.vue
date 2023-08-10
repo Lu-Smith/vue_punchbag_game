916478b0d2e5a08ee08138ef034a167e19cbbaaf
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-10 23:33:11 +0100

    Updated styling in MainHeader

--- punchbag_game/src/components/MainHeader.vue
+++ punchbag_game/src/components/MainHeader.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <button>Dark</button>
-    <button>Light</button>
+  <div class="Header">
+    <button class="darkButton">
+      Dark 
+      <span class="material-symbols-outlined">toggle_off</span>
+    </button>
+    <button class="lightButton">
+      Light 
+      <span class="material-symbols-outlined">toggle_on</span>
+</button>
   </div>
 </template>
 
@@ -10,5 +16,22 @@
 </script>
 
 <style>
+.Header {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+}
+
+.Header button {
+  padding: 5px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  font-weight: 600;
+}
+
 
 </style>
